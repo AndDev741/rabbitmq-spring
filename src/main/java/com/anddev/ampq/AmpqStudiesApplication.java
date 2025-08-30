35a761c364a750e3ce5a2ddb98f1baf96881d9d8
anddev741 <anddesenvolved@gmail.com>
2025-08-30 15:24:17 +0100

    feat: Simple Spring boot configuration for a rabbitmq application

---
+++ src/main/java/com/anddev/ampq/AmpqStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.anddev.ampq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmpqStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmpqStudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anddev/ampq/AmpqStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anddev.ampq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpqStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
